3168cd76b4a0d2275578b191c69cba584a40449e
PetrL <lipovskyp@seznam.cz>
2024-05-27 21:51:31 +0200

    S6-52-after-Diving-kostraWeb

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import "./style.css";
 
 function App(){
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-                <h1>Today I Learned</h1>
-            </div>
+    const appTitle = "Today I Learned"
 
-            <button className="btn btn-large btn-open">Share a fact</button>
-        </header>
+    return (
+        <>
+        {/* HEADER */}
+            <header className="header">
+                <div className="logo">
+                    <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+                    <h1>{appTitle}</h1>
+                </div>
+
+                <button className="btn btn-large btn-open">Share a fact</button>
+            </header>
+            <NewFactForm />
+            <main className="main">
+                <CategoryFilter />
+                <FactList />
+            </main>
+        </>
     );
 }
 
+function NewFactForm(){
+    return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+    return <aside>Category Filter</aside>
+}
+
+function FactList() {
+    return <section>Fact list</section>
+}
 
 export default App;
