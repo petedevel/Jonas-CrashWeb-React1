5c181edb1f133ef461b805e4f4384721ffdadd81
PetrL <lipovskyp@seznam.cz>
2024-05-28 15:42:49 +0200

    S6-54-after-Props+style.css

--- src/App.js
+++ src/App.js
@@ -84,33 +84,40 @@ function FactList() {
         <section>
             <ul className="facts-list">
                 {facts.map(fact=> (
-                    <li key={fact.id} className="fact">
-                        <p>
-                            {fact.text}
-                            <a
-                                className="source"
-                                href={fact.source}
-                                target="_blank"
-                            >
-                                (Source)
-                            </a>
-                        </p>
-                        <span className="tag" 
-                            style={{
-                                backgroundColor: CATEGORIES.find(
-                                    cat=>cat.name===fact.category).color}}
-                        >
-                            {fact.category}
-                        </span>
-                        <div className="vote-buttons">
-                            <button>👍 {fact.votesInteresting}</button>
-                            <button>🤯 {fact.votesMindblowing}</button>
-                            <button>⛔️ {fact.votesFalse}</button>
-                        </div>
-                    </li>
+                    <Fact key={fact.id} fact={fact} />
                 ))}
             </ul>
+            <p>Thera are {facts.length} in our database. Add your own!</p>
         </section>
+    );
+}
+
+function Fact({fact}) {
+    return (
+        <li className="fact">
+            <p>
+                {fact.text}
+                <a
+                    className="source"
+                    href={fact.source}
+                    target="_blank"
+                >
+                    (Source)
+                </a>
+            </p>
+            <span className="tag" 
+                style={{
+                    backgroundColor: CATEGORIES.find(
+                        cat=>cat.name === fact.category).color}}
+            >
+                {fact.category}
+            </span>
+            <div className="vote-buttons">
+                <button>👍 {fact.votesInteresting}</button>
+                <button>🤯 {fact.votesMindblowing}</button>
+                <button>⛔️ {fact.votesFalse}</button>
+            </div>
+        </li>
     );
 }
 
