ac18eee0533b2fdbb90d47d261c63631735ff81a
PetrL <lipovskyp@seznam.cz>
2024-06-01 15:25:27 +0200

    S7-68-end; before Production Build

--- src/App.js
+++ src/App.js
@@ -265,6 +265,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({fact, setFacts}) {
     const [isUpdating, setIsUpdating] = useState(false);
+    const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
     async function handleVote(columnName) {
         setIsUpdating(true);
         const { data: updatedFact, error } = await supabase
@@ -281,6 +282,7 @@ function Fact({fact, setFacts}) {
     return (
         <li className="fact">
             <p>
+                {isDisputed ? <span className="disputed">[DISPUTED]</span> :  null}
                 {fact.text}
                 <a
                     className="source"
